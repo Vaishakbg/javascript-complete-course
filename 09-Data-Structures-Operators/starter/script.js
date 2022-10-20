435e4cf71c25c9398ddcd40278fd7828f92d368e
Vaishak <vaishak.developer@gmail.com>
2022-10-20 19:27:41 +0530

    Codeing challenge 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -48,6 +48,133 @@ const restaurant = {
 };
 
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// /*
+////////////////////////////////////
+// START Looping Arrays: The for-of loop
+
+
+/*
+////////////////////////////////////
+// START Coding Challenge #1 
+
+// 1.
+const [player1, player2] = game.players;
+console.log(player1,player2);
+
+// 2.
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+// 3.
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+// 4.
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1, draw, team2);
+
+// 6.
+const printGoals = function(...players) {
+  console.log(players);
+  console.log(`${players.length} goals were scored`);
+}
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored)
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+////////////////////////////////////
+// END Coding Challenge #1 
+*/
+
+/*
+////////////////////////////////////
+// START Logical Assignment Operators
+
+const rest1 = {
+  name: 'A2a',
+  // numGuest: 20,
+  numGuest: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'John',
+};
+
+//OR assignment operator///////////////////
+// rest1.numGuest = rest1.numGuest || 10;
+// rest2.numGuest = rest2.numGuest || 10;
+// rest1.numGuest ||= 10;
+// rest2.numGuest ||= 10;
+
+//Nullish assignment operator (null or undefined)///////////////////
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+
+//AND assignment operator///////////////////
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+////////////////////////////////////
+// END Logical Assignment Operators
+*/
+
+
 /*
 ////////////////////////////////////
 // START The Nullish Coalescing Operator (??)
